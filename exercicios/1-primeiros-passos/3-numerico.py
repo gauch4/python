3808e673fd95c8b392020b64c05cb1bc1285f46d
Nayra Rodrigues <60489752+gauch4@users.noreply.github.com>
2025-09-05 20:35:16 +0000

    numericos-ok-2

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,33 @@ idade_numerica = 7
 altura = 1.60    
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(idade_numerica))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+idade = 10 + 3
+print(idade)
 
 # Realize uma operação entre dados do tipo inteiro e float
+soma = idade + 2.3
+print(soma)
+print(type(soma))
+
+divisao = 6/3
+print(divisao)
+
+decimal = 50/8
+print(decimal)
+
+print(int(decimal))
+
+round(decimal,1)
+
+round(decimal,0)
+
+
+nome = "Sofia"
+print(nome*3)
+
+print(altura)
+
+print(data_nascimento)
